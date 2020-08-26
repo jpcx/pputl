e5def2c5e362c9faa567b0e58b5f348690cba5c4
Justin Collier <m@jpcx.dev>
2020-08-26 12:00:58 -0700

    test/main.cc: removed README example and added license text

--- test/main.cc
+++ test/main.cc
@@ -1,17 +1,31 @@
-#include <pputl/pputl.h>
-
-#define THREE PPUTL_SUM(1, 1, 1)
-
-#if PPUTL_NEQ(THREE, 3)
-static_assert(false);
-#endif
-
-#define MULTICAT PPUTL_CAT(my, cool, cat)
-
-#define SUM_REDUCER(accum, val, i) PPUTL_ADD(a, v)
-#define SUM(...) PPUTL_REDUCE(SUM_REDUCER, 0, __VA_ARGS__)
-
-#define SEVEN SUM(1, 2, 4)
+/////                                                                      c++20
+////////////////////////////////////////////////////////////////////////////////
+/// @file
+//                          __    ___
+//                         /\ \__/\_ \
+//   _____   _____   __  __\ \ ,_\//\ \
+//  /\ '__`\/\ '__`\/\ \/\ \\ \ \/ \ \ \
+//  \ \ \_\ \ \ \_\ \ \ \_\ \\ \ \_ \_\ \_
+//   \ \ ,__/\ \ ,__/\ \____/ \ \__\/\____\
+//    \ \ \   \ \ \   \/___/   \/__/\/____/
+//     \/_/    \/_/
+//
+//  pputl Preprocessor Utilities
+//  Copyright (C) 2020 Justin Collier <m@jpcx.dev>
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the internalied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//                                                                            //
+//  You should have received a copy of the GNU General Public License        ///
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.  ////
+////////////////////////////////////////////////////////////////////////////////
 
 #define CCTEST_MAIN
 #include <cctest/cctest.h>
