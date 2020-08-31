1b73ee3ea8e14649b09dd616cee975142d63d8ce
Justin Collier <m@jpcx.dev>
2020-08-31 08:11:22 -0700

    rebuilt for 0.2.1

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['pputl_200_2e2_2e0_20_7c_20_3ca_20href_3d_22http_3a_2f_2fjpcx_2egithub_2eio_2fpputl_2fgroup_5f_5fpputl_2ehtml_22_3e_3cem_3eapi_20docs_3c_2fem_3e_3c_2fa_3e_20_7c_20_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2fjpcx_2fpputl_2fblob_2f0_2e2_2e0_2fchangelog_2emd_22_3e_3cem_3echangelog_3c_2fem_3e_3c_2fa_3e_27',['pputl 0.2.0 | &lt;a href=&quot;http://jpcx.github.io/pputl/group__pputl.html&quot;&gt;&lt;em&gt;API Docs&lt;/em&gt;&lt;/a&gt; | &lt;a href=&quot;https://github.com/jpcx/pputl/blob/0.2.0/CHANGELOG.md&quot;&gt;&lt;em&gt;CHANGELOG&lt;/em&gt;&lt;/a&gt;',['../index.html',1,'']]],
+  ['pputl_200_2e2_2e1_20_7c_20_3ca_20href_3d_22http_3a_2f_2fjpcx_2egithub_2eio_2fpputl_2fgroup_5f_5fpputl_2ehtml_22_3e_3cem_3eapi_20docs_3c_2fem_3e_3c_2fa_3e_20_7c_20_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2fjpcx_2fpputl_2fblob_2f0_2e2_2e1_2fchangelog_2emd_22_3e_3cem_3echangelog_3c_2fem_3e_3c_2fa_3e_27',['pputl 0.2.1 | &lt;a href=&quot;http://jpcx.github.io/pputl/group__pputl.html&quot;&gt;&lt;em&gt;API Docs&lt;/em&gt;&lt;/a&gt; | &lt;a href=&quot;https://github.com/jpcx/pputl/blob/0.2.1/CHANGELOG.md&quot;&gt;&lt;em&gt;CHANGELOG&lt;/em&gt;&lt;/a&gt;',['../index.html',1,'']]],
   ['pputl_28',['pputl',['../group__pputl.html',1,'']]],
   ['pputl_2eh_29',['pputl.h',['../pputl_8h.html',1,'']]],
   ['pputl_5fadd_30',['PPUTL_ADD',['../group__pputl.html#ga8aff7f50065fd7cee58770f710742494',1,'add.h']]],
